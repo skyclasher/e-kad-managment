9ffe0e39b39234a9f6b8dc015cfb6ba5a824dfe2
Ahmad Syafiq <asyafiq99@gmail.com>
2019-07-08 20:08:47 +0800

    init

---
+++ Pages/Settings/Account.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECardManagment.Data;
+using ECardManagment.Services.Mail;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ECardManagment.Pages.Settings
+{
+    public class AccountModel : PageModel
+    {
+        public AccountModel() 
+        {
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
---
+++ Pages/Settings/History.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECardManagment.Data;
+using ECardManagment.Services.Mail;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ECardManagment.Pages.Settings
+{
+    public class HistoryModel : PageModel
+    {
+        public HistoryModel() 
+        {
+        }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
